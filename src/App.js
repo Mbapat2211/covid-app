2d221c7a6a7aa0050cd333e0ee3586b8d6d32374
Mihir Bapat <22mihirbapat11@gmail.com>
2021-05-24 17:58:22 +0300

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+	return (
+  		<body class="App-Body">
+			<header id="App-Header">
+				<div class="App-Container">
+					<h1>Covid Resource App</h1>
+				</div>
+			</header>
+
+			<section id="App-Section-0">
+				<div class="App-Container">Getting Started...</div>
+			</section>
+			
+			<section id="App-Section-1">
+				<div class="App-Container">
+					<h3 class="head">For Resource Seekers</h3>
+					<p1 class="body">To search for information regarding covid related resources</p1>
+					<br></br><br></br><br></br>	
+					<button class="button">Continue...</button>				
+				</div>
+			</section>
+
+			<section id="App-Section-2">
+				<div class="App-Container">
+					<h3 class="head">For Resource Sellers</h3>
+					<p1 class="body">To provide information covid related resources</p1>
+					<br></br><br></br><br></br><br></br>
+					<button class="button">Continue...</button>				
+				</div>
+			</section>
+		</body>
+	);
 }
 
 export default App;
